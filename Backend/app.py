a07e29970770d096f5dd7f56249913bf7b9be7a6
lowkanta <ricardosiqueirarf@gmail.com>
2026-04-08 15:46:19 -0300

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -292,8 +292,8 @@ app.register_blueprint(sistemas_bp)
 app.register_blueprint(trilhos_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 
 
