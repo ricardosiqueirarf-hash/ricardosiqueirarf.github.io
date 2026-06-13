711389a90a0133db33f0240c9ec5f146eb48c406
lowkanta <ricardosiqueirarf@gmail.com>
2026-06-13 02:56:34 -0300

    Finalize PROMOB MVP app

--- Backend/pricing-correr.js
+++
@@ -1,8 +0,0 @@
-function calcularPrecoPortaCorrer() {
-    const quantidadePortas = +document.getElementById("quantidade")?.value || 1;
-    const valorAdicional = +document.getElementById("valor_adicional")?.value || 0;
-    const total = calcularPrecoBasePorta() + calcularValorSistemaETrilhos() + valorAdicional;
-    return total * quantidadePortas;
-}
-
-window.calcularPrecoPortaCorrer = calcularPrecoPortaCorrer;
